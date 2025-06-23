976fca2e2cb791b3803ffea2f01fe0bc9c0db0ee
solomcbride25 <solomcbride22@gmail.com>
2025-06-23 01:17:24 -0400

    Fix: import registerUser and other controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const { registerUser, login, logout } = require('../controllers/userControllers');
+
 router.post("/register", registerUser)
 router.post("/login", login)
 router.post("/logout", logout)
